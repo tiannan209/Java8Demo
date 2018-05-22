0176eacc334ba50d2d9c7b1210b3481e4141f9fe
Nan Tian <tiannan209@hotmail.com>
2018-05-22 11:19:26 +0800

    Java 8 new feature study

--- src/LambdaDemo.java
+++ src/LambdaDemo.java
@@ -14,6 +14,14 @@ public class LambdaDemo {
         System.out.println("10 - 5 = " + subtraction.operation(10, 5));
         System.out.println("10 x 5 = " + multiplication.operation(10, 5));
         System.out.println("10 / 5 = " + division.operation(10, 5));
+
+        // without parenthesis
+        GreetingService greetingService1 = message -> System.out.println("Hello without parenthesis: " + message);
+        greetingService1.greet("Tian Nan");
+
+        // with parenthesis
+        GreetingService greetingService2 = (message) -> System.out.println("Hello with parenthesis: " + message);
+        greetingService2.greet("Tony Li");
     }
 
     interface MathOperation {
